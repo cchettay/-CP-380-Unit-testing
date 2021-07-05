f48ddb1ea667e4f7c9a0e474f31d8925ad8f391c
cchettay1995 <chinchujoseph02@gmail.com>
2021-07-05 14:44:24 -0400

    Completed Unit Testing

--- IsItALeapYear/Program.cs
+++ IsItALeapYear/Program.cs
@@ -14,23 +14,21 @@ namespace IsItALeapYear
         }
         public Boolean IsItALeapYear(int year)
         {
-            if (year % 400 == 0)
+            if (year % 4 == 0)
             {
+
                 if (year % 100 == 0)
                 {
-                    if (year % 4 == 0)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    
                 }
                 else
                 {
                     return true;
                 }
+            }
+            if (year % 400 == 0)
+            {
+                return true;
             }
             else
             {
